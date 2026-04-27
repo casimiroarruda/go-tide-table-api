ed4d185b61a1ca82a7d8dee1cebaf3e2b0398794
Anderson Casimiro <casimiro.arruda@gmail.com>
2026-04-27 10:48:17 -0300

    refactor: change client_id column type from TEXT to UUID in auth_store schema

--- database/migrations/20260426220232_create_auth_schema.up.sql
+++ database/migrations/20260426220232_create_auth_schema.up.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS auth_store;
 
 CREATE TABLE auth_store.clients (
-    client_id TEXT PRIMARY KEY,
+    client_id UUID PRIMARY KEY,
     client_secret TEXT NOT NULL, -- Hash Bcrypt
     name TEXT NOT NULL,
     scopes TEXT[] NOT NULL,      -- Array de strings do Postgres
